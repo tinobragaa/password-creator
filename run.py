4268af5f98ca52e62301ca9e7b0fc01436f85f35
Valentino Braga <valentinobragaa@gmail.com>
2023-04-12 22:56:17 +0000

    Add welcome_message function and import random/time/colorama modules

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Password Creator: Password generator created for my PP3.
+
+import random
+
+import time
+
+from colorama import Fore, Style
+
+def welcome_message():
+    """
+    This function showcase the welcome message,
+    logo design, explain what the program is
+    and lead to the get_user_name function.
+    """
+    print(Fore.WHITE + "==============================================")
+    time.sleep(.5)
+    print(Fore.WHITE + "\nWecome to the password creator!\n")
+    print(Fore.WHITE + "Create a unique password with a hard-to-hack tech.\n")
+    print(Fore.WHITE + "==============================================")
+
+welcome_message()
