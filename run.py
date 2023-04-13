a9d787695729cac54d29db4931c31e40891ae45f
Valentino Braga <valentinobragaa@gmail.com>
2023-04-13 01:16:26 +0000

    Add typewriter_print function and extra information to welcome_message function

--- run.py
+++ run.py
@@ -1,10 +1,21 @@
-#Password Creator: Password generator created for my PP3.
-
+# Import necessary modules.
+import pyfiglet
 import random
-
 import time
-
+import sys
+from time import sleep
 from colorama import Fore, Style
+
+# Code reference from https://stackoverflow.com/questions/20302331/typing-effect-in-python
+def typewriter_print(text, speed=0.03):
+    '''
+    This function add the typewriter effect.
+    '''
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(speed)
+    print("\n")
 
 def welcome_message():
     """
@@ -12,10 +23,33 @@ def welcome_message():
     logo design, explain what the program is
     and lead to the get_user_name function.
     """
-    print(Fore.WHITE + "==============================================")
+    print(
+        Fore.CYAN
+        + "===================================="
+        + "==============================\n"
+    )
+    print(Style.RESET_ALL)
+    print(pyfiglet.figlet_format("Password", font="larry3d"))
+    print(pyfiglet.figlet_format("Creator", font="larry3d"))
+    print(
+        Fore.CYAN
+        + "===================================="
+        + "=============================="
+    )
+    print(Style.RESET_ALL)
     time.sleep(.5)
-    print(Fore.WHITE + "\nWecome to the password creator!\n")
-    print(Fore.WHITE + "Create a unique password with a hard-to-hack tech.\n")
-    print(Fore.WHITE + "==============================================")
+    typewriter_print("Welcome to the password creator!")
+    typewriter_print(
+        "Did you know it only takes seconds to hack "
+        + "small passwords?"
+    )
+    typewriter_print(
+        "We provide a secure password, all we need is that you "
+        + "follow two steps and choose:"
+    )
+    typewriter_print("1 - Between 8 and 64 characters long.")
+    typewriter_print(
+        "2 - Letters only or mixed characters (letters/numbers/symbols)."
+    )
 
 welcome_message()
